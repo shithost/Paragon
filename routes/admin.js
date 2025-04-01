f55e711f1bbb6d9b68ffecd9b2885891235400fc
root <root@play451.local>
2025-04-01 02:05:27 +0000

    added a admin user manager page thats not done and edit sum css

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,27 @@
 const express = require('express');
 const isAuthenticated = require('../middleware/auth');
+const fs = require('fs').promises;
+const path = require('path');
 
 const router = express.Router();
 
-router.get('/admin/home', isAuthenticated, (req, res) => {
-    res.render('admin', { title: 'Paragon' });
+router.get('/admin/home', isAuthenticated, async (req, res) => {
+    try {
+        const data = await fs.readFile(path.join(__dirname, '../users.json'), 'utf8');
+        
+        const users = JSON.parse(data);
+        
+        const totalUsers = users.length;
+        
+        res.render('admin', { title: 'Paragon', totalUsers });
+    } catch (err) {
+        console.error('Error reading users.json:', err);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+router.get('/admin/users', isAuthenticated, (req, res) => {
+    res.render('admin-users', { title: 'Paragon' });
 });
 
 module.exports = router;
